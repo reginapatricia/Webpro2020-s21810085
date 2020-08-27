49920d7df068d7f8eb0cda6bfc4bc7d3529f290b
reginapatricia <s21810085@student.unklab.ac.id>
2020-08-27 12:19:23 +0800

    third commit

---
+++ pertemuan_3/index.js
@@ -0,0 +1,30 @@
+/* 
+    1. Variabel dan tipe data 
+*/
+
+let firstName ="Regina";
+let lastName ="Mailangkay";
+const age =19;
+const isStudent = true;
+
+console.log("My name is " +firstName, lastName + ",i'm " +age + "years old");
+console.log("My last name is " +lastName);
+console.log(age);
+
+console.log(typeof firstName);
+
+function helloWorld() {
+    var hello="helloWorld"
+    console.log(hello);
+}
+
+helloWorld();
+
+
+/*
+    2. Operator
+ */
+
+ let bilangan =10;
+ console.log(bilangan=="10"); //loose equality dipaksa convert
+ console.log(bilangan ==="10"); //strict equality
