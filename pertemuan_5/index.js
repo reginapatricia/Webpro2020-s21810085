5962fd3e30a70af9e2f299f7a92efe2c2a5ff8c0
reginapatricia <s21810085@student.unklab.ac.id>
2020-09-03 14:59:37 +0800

    fifth commit

--- pertemuan_5/index.js
+++ pertemuan_5/index.js
@@ -3,13 +3,14 @@
     ARRAY 
 
 */
-
-//cara mendeklarasikan array
-//let angka =[1,2,3,4,5,6,7,8];
-//console.log(angka);
-//let buah = ["mangga","apel", "pir" ];
-//console.log(buah);
-
+/*
+cara mendeklarasikan array
+let angka =[1,2,3,4,5,6,7,8];
+console.log(angka);
+let buah = ["mangga","apel", "pir" ];
+console.log(buah);
+*/
+/*
 let arr =["text", 1, true];
 console.log(arr);
 
@@ -25,3 +26,69 @@ console.log(arr2[4][2]);
 
 console.log(typeof arr2);
 console.log(arr2.length);
+*/
+
+//manipulasi array
+//1. menambah isi array
+
+//method pada array
+//1. length
+//2.toString (mengubah array menjadi string)
+let mhs = ["john", "bob", "jane", "peter"];
+console.log(mhs.toString());
+
+//3. join
+
+console.log(mhs.join(" - "))
+
+//4. push, pop, unshift, shift
+// push untuk menambahkan element dibelakangnya
+ 
+mhs.push("mayrra");
+mhs.push ("regina");
+console.log(mhs.join (" - "));
+
+//pop untuk menghapus
+mhs.pop();
+mhs.pop();
+console.log(mhs.join (" - "));
+
+//unshift untuk menambahkan element didepan
+
+mhs.unshift("regina");
+console.log(mhs.join (" - "));
+
+//shit untuk menghapus yang paling depan
+
+mhs.shift();
+console.log(mhs.join (" - "));
+
+
+//5. concat
+// let buahh = ["mangga", "apel", "anggur"];
+// let sayur = ["kangkung", "bayam", "buncis"];
+
+// let makanan = buahh.concat(sayur);
+// console.log(makanan);
+
+//6. splice dan slice
+//splice untuk menambahkan element diantara element yang ada
+// let buah =["pisang","mangga", "apel", "jeruk"];
+// buah.splice(2, 0, "melon","semangka");
+// console.log(buah.join (" - "));
+
+//slice untuk mengambil element untuk ditaro di array baru
+
+// let buah2 = buah.slice(0, 3);
+// console.log(buah2.join (" - "));
+
+//7.forEach dan map
+//forEach
+
+let angka = [1,2,3,4,5,6,7,8];
+angka.forEach(function(el){
+    console.log(el);            //callback function
+})
+
+
+
