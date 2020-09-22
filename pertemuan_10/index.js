c0942f37898a31747e2813df7dedf8164b721ed7
reginapatricia <s21810085@student.unklab.ac.id>
2020-09-22 13:52:30 +0800

    tenth commit

---
+++ pertemuan_10/index.js
@@ -0,0 +1,49 @@
+//var let const
+
+// let nama= "regina";
+// let nama = "patricia";
+
+// console.log(nama);
+
+/* scope dalam javascript 
+1. function scope (var)
+2. block scope
+*/
+// function tes(){
+//     for (var i =0; i<10; i++){
+//         console.log(i);         //agar tidak bisa diakses dari luar
+//     }
+// }
+// tes();
+
+// //IIFE : fungsi yang langsung dipanggil. fungsinya ada didalam kurung
+// //contoh IIFE
+(function tes(){
+    for (var i =0; i<10; i++){
+        console.log(i); 
+    }
+})()
+
+// let i;
+// for (let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+// console.log(i);
+
+const person = {
+    firstName : "regina",
+    lastName : "mailangkay",
+    age : 19,
+};
+
+// console.log(
+//     "halo nama saya "+person.firstName +
+//     " " +person.lastName +
+//     ". umur saya adalah "+person.age+" tahun."
+// );
+
+//dipastikan memakai cara yang dibawah
+
+console.log(
+    `halo nama saya ${person.firstName} ${person.lastName} dan berumur ${person.age}` 
+);
