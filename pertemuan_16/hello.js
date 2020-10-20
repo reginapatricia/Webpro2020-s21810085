cc6a26f76e1ade4eb6b4f3b1640e60efb1c6683a
reginapatricia <s21810085@student.unklab.ac.id>
2020-10-20 09:09:24 +0800

    sixteenth

---
+++ pertemuan_16/hello.js
@@ -0,0 +1,7 @@
+import {word} from './component/word.js';
+
+export const hello = (user) => {
+    console.log(`Hello, ${user}`);
+}
+
+export default hello;
---
+++ pertemuan_16/index.js
@@ -0,0 +1,8 @@
+// import{ hello } from './hello.js'
+// import * as helper from './hello.js'
+
+// helper.world();
+// console.log(helper.numbers);
+
+import func1 from './hello.js'
+func1('Regina Mailangkay');
