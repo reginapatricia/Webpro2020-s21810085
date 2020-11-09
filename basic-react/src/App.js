4cf8e7acbaffd5200c04e122c278b34f58030d52
reginapatricia <s21810085@student.unklab.ac.id>
2020-11-10 03:37:43 +0800

    react app commit

---
+++ basic-react/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./components/Header"
+import Hello from "./components/Hello"
+import Content from "./components/Content";
+
+function App() {
+  return (
+   <>
+
+     <Header/>
+     <Hello/>
+     <Content/>
+
+   </>
+  );
+}
+
+export default App;
